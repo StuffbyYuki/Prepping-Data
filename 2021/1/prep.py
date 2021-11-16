4a8e2bc111f4c93d8f5f339ad80f50d61b43b783
stuffbyyuki <kake.19940627@gmail.com>
2021-11-15 17:14:45 -0700

    added pathlib and modified code

--- 2021/1/prep.py
+++ 2021/1/prep.py
@@ -2,15 +2,16 @@
 # To run program: python 2021/1/prep.py
 # Author: Yuki Kakegawa
 
+from pathlib import Path
 import pandas as pd
 import numpy as np
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', 20)
 
 # read in the data
-PATH = './2021/1/'
-FILE = 'PD 2021 Wk 1 Input.xlsx'
-df = pd.read_excel(PATH + FILE)
+BASE_DIR = Path(__file__).resolve().parent#.parent
+INPUT_FILE_PATH = BASE_DIR.joinpath('PD 2021 Wk 1 Input.xlsx')
+df = pd.read_excel(INPUT_FILE_PATH)
 
 # helper functions
 def str_before_hyphen(string):
@@ -28,8 +29,10 @@ df.drop(columns=['Store - Bike'], inplace=True)
 df['Quarter'] = df['Date'].dt.quarter 
 df['Day of Month'] = df['Date'].dt.day 
 df.drop(columns=['Date'], inplace=True)
-
 df = df.iloc[10:,:]
 
-df.to_csv(PATH+'output.csv', index=None)
+#output to a csv
+OUTPUT_FILE_PATH = BASE_DIR.joinpath('output.csv')
+df.to_csv(OUTPUT_FILE_PATH, index=None)
+print('Output file saved to', OUTPUT_FILE_PATH)
 
