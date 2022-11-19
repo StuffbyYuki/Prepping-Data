4233d16138d6c53c04d35104ff196a14c74fd77b
Yuki Kakegawa <ykakegawa@Yuki-Kakegawas-MacBook-Pro.local>
2022-11-18 17:08:44 -0700

    added more packages

--- 2022/week1/main.py
+++ 2022/week1/main.py
@@ -1,3 +1,10 @@
+import os
+
+#USE ONLY ONE OF THESE:
+
+os.environ["MODIN_ENGINE"] = "ray"  # Modin will use Ray
+os.environ["MODIN_ENGINE"] = "dask"  # Modin will use Dask
+
 import modin.pandas as pd
 
 df = pd.read_csv('https://drive.google.com/file/d/1p8gt3cR3ATCeGK81pnT90x0a6dbCXst1/view')
