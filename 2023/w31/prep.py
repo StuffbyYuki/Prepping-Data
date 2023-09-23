fbba3c1f33655568b91431aa6833ad55430c5004
stuffbyyuki <kake.19940627@gmail.com>
2023-09-22 23:22:59 -0600

    code logic done

---
+++ 2023/w31/prep.py
@@ -0,0 +1,60 @@
+import polars as pl
+
+dim_lf = pl.scan_csv('data/input/ee_dim_input.csv')
+monthly_lf = pl.scan_csv('data/input/ee_monthly_input.csv')
+
+def create_lookup_table():
+    '''
+    '''
+    dim_id_guid_combinations = (dim_lf.select(pl.col(['employee_id', 'guid'])))
+    monthly_id_guid_combinations = (monthly_lf.select(pl.col(['employee_id', 'guid'])))
+    lookup_table = (
+        pl.concat([dim_id_guid_combinations, monthly_id_guid_combinations], how='vertical')
+        .drop_nulls()
+        .unique()
+        .collect()
+    )
+    
+    return lookup_table
+
+def fill_emp_and_guid(lf: pl.LazyFrame, guid_lookup_dict: dict, emp_lookup_dict: dict) -> pl.LazyFrame:
+    ''''
+    fill employee_id and guid with a lookup dictionary
+    '''
+    res_lf = (
+        lf
+        .with_columns(pl.col('employee_id').fill_null(pl.col('guid').map_dict(guid_lookup_dict)))
+        .with_columns(pl.col('guid').fill_null(pl.col('employee_id').map_dict(emp_lookup_dict)))
+    )
+    return res_lf
+
+def main():
+    
+    lookup_table = create_lookup_table()
+
+    emp_lookup_dict = dict(
+        zip(
+            lookup_table.select('employee_id').to_series(),
+            lookup_table.select('guid').to_series()
+        )
+    )
+
+    guid_lookup_dict = dict(
+        zip(
+            lookup_table.select('guid').to_series(),
+            lookup_table.select('employee_id').to_series()
+        )
+    )
+
+    dim_null_filled_lf = (
+        dim_lf
+        .pipe(fill_emp_and_guid, emp_lookup_dict, guid_lookup_dict)
+    )
+
+    monthly_null_filled_lf = (
+        monthly_lf
+        .pipe(fill_emp_and_guid, emp_lookup_dict, guid_lookup_dict)
+    )
+
+    dim_null_filled_lf.sink_csv('data/output/ee_dim_output.csv')
+    monthly_null_filled_lf.sink_csv('data/output/ee_monthly_output.csv')
