5ec081ccadebc136dcdf48470ae9e0109b1b6c2d
stuffbyyuki <kake.19940627@gmail.com>
2023-09-23 22:43:07 -0600

    test done

--- 2023/w31/test.py
+++
@@ -1,9 +0,0 @@
-import polars as pl
-from polars.testing import assert_frame_equal
-
-ee_dim_output = pl.read_csv('data/output/ee_dim_output.csv')
-ee_monthly_output = pl.read_csv('data/output/ee_monthly_output.csv')
-ee_dim_outout_correct = pl.read_csv('data/output/test/ee_dim_v2.csv')
-ee_monthly_output_correct = pl.read_csv('data/output/test/ee_monthly_v2.csv')
-
-assert_frame_equal(ee_dim_output, ee_dim_outout_correct, check_row_order=False)
