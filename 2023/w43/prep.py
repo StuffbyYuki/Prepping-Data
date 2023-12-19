04ec615c0a696e96d576904cffece67a021cd5b9
stuffbyyuki <kake.19940627@gmail.com>
2023-12-19 08:30:51 -0700

    w43 updated

--- 2023/w43/prep.py
+++ 2023/w43/prep.py
@@ -1,28 +1,67 @@
 import polars as pl
 
-clubs_df = pl.read_csv('data/After School Club.csv')
-events_df = pl.read_csv('data/Events.csv', try_parse_dates=True)
-
-weekdays_mapping = {1: 'Monday', 2: 'Tuesday', 3: 'Wednesday', 4: 'Thursday', 5: 'Friday'}
-
-df = (
-    events_df
-    .set_sorted('Date')
-    .upsample(
-        time_column='Date', every='1d', maintain_order=True
-    )
+attendance_df = (
+    pl.read_csv('data/Term*.csv')
     .with_columns(
-        pl.col('Date')
-        .dt.weekday()
-        .map_dict(weekdays_mapping)
-        .alias('Day of Week'),
-        pl.col('Event').fill_null('N/A')
-    )
-    .filter( 
-        pl.col('Day of Week').is_not_null() 
-    )
-    .join(
-        clubs_df, on='Day of Week', how='left'
+        (pl.col('First Name') + ' ' + pl.col('Last Name')).alias('Full Name')
     )
 )
-print(df)
+
+year_group_df = pl.read_csv('data/Year Groups.csv')
+
+attendance_yearly_df = (
+    attendance_df
+    .join(
+        year_group_df, how='inner', on=['First Name', 'Last Name']
+    )
+    .group_by('Full Name', 'Year Group')
+    .agg(
+        pl.col('Days Present').sum(),
+        pl.col('Days Absent').sum(),
+        (
+            pl.col('Days Present').sum() / 
+            (pl.col('Days Present').sum() + pl.col('Days Absent').sum() ) *
+            100
+        ) 
+        .round(2) 
+        .alias('Year Attendance Rate')
+    )
+    .with_columns(
+        pl.col('Year Attendance Rate').rank('dense', descending=True).alias('Rank')
+    )
+)
+
+best_attendance_rate = (
+    attendance_yearly_df
+    .filter(pl.col('Rank')==1)
+    .select('Full Name', 'Year Attendance Rate', 'Rank')
+)
+print(best_attendance_rate)
+
+top_5_perc_list_df =  (
+    attendance_yearly_df
+    .sort('Rank')
+    .group_by('Year Group')
+    .agg(
+        pl.col('Year Group', 'Year Attendance Rate')
+        .head(
+            (pl.count() * 0.05)
+            .ceil()
+        )
+    )
+)
+
+top_5_attendance = (
+    attendance_yearly_df
+    .join(top_5_perc_list_df, how='inner', on='Year Group', suffix=' List')
+    .filter(
+        pl.col('Year Attendance Rate').is_in(pl.col('Year Attendance Rate List'))
+    )
+    .select('Year Group', 'Full Name', 'Year Attendance Rate')
+)
+print(top_5_attendance)
+
+
+
+
+
